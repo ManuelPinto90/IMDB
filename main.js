fa19750491735f6cc2be5786529db010aa4bc33f
ManuelPinto90 <79107003+ManuelPinto90@users.noreply.github.com>
2021-02-18 11:57:59 +0100

    claseProfessionals

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+//Archivo principal codigo
+var claseProfessional_1 = require("./claseProfessional");
+var actor1 = new claseProfessional_1.Professional("Leonardo di Caprio", 48, "Male", 80, 1.82, "Blondy", "Blue", "Aria", false, "Yankee", 12, "Actor");
+console.log(actor1.showProfessional());
--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
 //Archivo principal codigo
+import {Professional}from"./claseProfessional"
+let actor1 : Professional = new Professional("Leonardo di Caprio",48,"Male",80,1.82,"Blondy", "Blue", "Aria", false, "Yankee",12,"Actor")
+console.log(actor1.showProfessional())
