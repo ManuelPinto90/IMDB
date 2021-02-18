9b12a94233937a9dc8e2c6bd10974d3a9b1693c8
ManuelPinto90 <79107003+ManuelPinto90@users.noreply.github.com>
2021-02-18 12:08:37 +0100

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-//Archivo principal codigo
-var claseProfessional_1 = require("./claseProfessional");
-var actor1 = new claseProfessional_1.Professional("Leonardo di Caprio", 48, "Male", 80, 1.82, "Blondy", "Blue", "Aria", false, "Yankee", 12, "Actor");
-console.log(actor1.showProfessional());
