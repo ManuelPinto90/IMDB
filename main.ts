929bb6a66b95b85de585772cd85036d36039cb15
ManuelPinto90 <79107003+ManuelPinto90@users.noreply.github.com>
2021-02-18 12:24:36 +0100

    Modificacion nombre variable

--- main.ts
+++ main.ts
@@ -5,5 +5,5 @@ import { Movie } from "./claseMovie";
 let starwars: Movie = new Movie("Star Wars", 1979, "EEUU", "Sci-fy")
 console.log(starwars.showMovie());
 
-let actor1 : Professional = new Professional("Leonardo di Caprio",48,"Male",80,1.82,"Blondy", "Blue", "Aria", false, "Yankee",12,"Actor")
-console.log(actor1.showProfessional())
+let diCaprio : Professional = new Professional("Leonardo di Caprio",48,"Male",80,1.82,"Blondy", "Blue", "Aria", false, "Yankee",12,"Actor")
+console.log(diCaprio.showProfessional())
