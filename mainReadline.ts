44cd26d82db779ee0b8f88ee7b962fa00d6969fe
Bermejo3 <alberto.bermejo.corredor@gmail.com>
2021-02-19 10:11:06 +0100

    Ejercicio finalizado

--- mainReadline.ts
+++ mainReadline.ts
@@ -1,36 +1,23 @@
+import { IMDB } from "./claseIMDB";
 import { Movie } from "./claseMovie";
 
-const readline = require('readline')
+var readlineSync = require('readline-sync');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+var titulo = readlineSync.question('¿Cual es el título? ');
+var releaseYear = readlineSync.question('¿En qué año se estrenó?');
+var nationality = readlineSync.question('¿De qué pais?')
+var genre = readlineSync.question('¿De qué genero es?')
 
-rl.question('¿Cómo se llama la pelicula?', (answer1) => {
-        const titulo = answer1;
-        rl.question('¿En que año se estrenó?', (answer2) => {
-            const anyo = answer2;
-            rl.question('¿De qué país es?', (answer3) => {
-                const nacionalidad = answer3  
-                rl.question('¿De qué genero es?', (answer4) => {
-                    const genero = answer4
-                    const nuevaPelicula: Movie = new Movie(titulo, anyo, nacionalidad, genero)
-                    console.log(nuevaPelicula);
-                    rl.close();
-                });
-                rl.close();
-                
-            });
-            rl.close();
-        });
-        rl.close();
-        
-    });
+let nuevaPeli: Movie = new Movie(titulo, releaseYear, nationality, genre)
 
-    
-    
+let nuevoIMDB: IMDB = new IMDB([])
+nuevoIMDB = nuevoIMDB.obtenerInstaciaIMDB("imdbBBDD.json")
 
+nuevoIMDB.peliculas.push(nuevaPeli)
+
+console.log(nuevoIMDB);
+
+nuevoIMDB.escribirEnFicheroJSON("imdbBBDD.json")
 
 
 
